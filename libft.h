0dafe899a364cd1b66c8c1cb515b271564655c1d
Valimero <valimero.ilou@gmail.com>
2015-11-08 19:05:50 +0100

    patch

---
+++ libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h> /* for function write() 	*/
+#include <stdlib.h> /* malloc() and free() 	*/
+#include <string.h> /* size_t 				*/
+
+void		ft_putchar(char c); /*ok*/
+void		ft_putstr(char *str);
+
+#endif
